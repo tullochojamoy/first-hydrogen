35269f66da0e57ed482e8f6c6ca291ee6c2c5a82
Jamoy Tulloch <tullochojamoy@gmail.com>
2023-02-06 09:53:19 -0500

    Message

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -4,19 +4,11 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain:
-      // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
-    storefrontToken:
-      // @ts-ignore
-      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
-    privateStorefrontToken:
-      // @ts-ignore
-      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
+    storeDomain: 'digitalresource876.myshopify.com',
+    storefrontToken: '9501eb0e81897ee77025c1c279a38684',
+    // privateStorefrontToken: Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2022-07',
-    // @ts-ignore
-    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
+    // storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
   },
   session: CookieSessionStorage('__session', {
     path: '/',
--- src/components/global/FooterMenu.client.jsx
+++ src/components/global/FooterMenu.client.jsx
@@ -4,6 +4,7 @@ import {Link} from '@shopify/hydrogen';
 
 import {Heading, IconCaret} from '~/components';
 
+import {useState} from 'react';
 /**
  * A server component that specifies the content of the footer on the website
  */
@@ -13,6 +14,29 @@ export function FooterMenu({menu}) {
     nav: 'grid gap-2 pb-6',
   };
 
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = document.getElementById('form');
+      const formData = new FormData(form);
+    fetch('https://usebasin.com/f/f94575377d19', {
+      method: 'POST',
+        headers: {
+        Accept: 'application/json',
+      },
+        body: formData,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("success");
+        } else {
+          console.log("fail");
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <>
       {(menu?.items || []).map((item) => (
@@ -21,6 +45,19 @@ export function FooterMenu({menu}) {
             {/* @ts-expect-error @headlessui/react incompatibility with node16 resolution */}
             {({open}) => (
               <>
+                <h6>Subscribe to our NewsLetter</h6>
+                <form id="form" onSubmit={handleSubmit}>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                  />
+                  <button type="submit">Submit</button>
+                </form>
+
                 <Disclosure.Button className="text-left md:cursor-default">
                   <Heading className="flex justify-between" size="lead" as="h3">
                     {item.title}
